7496feaf8d85d9b1244effb3a050a06f69352b2c
Nicherus <joaonichev@gmail.com>
2021-09-25 00:43:32 -0300

    feat: add layer of security checking id of contact with sender

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -8,7 +8,7 @@ export const verifyJWT = async (request: any, response: Response, next: NextFunc
     response.status(401).send({error: "token needed"});
   } else {
     jwt.verify(token, process.env.SECRET, (err: any, decoded: { id: any; }) => {
-      if (err) response.status(401).send({error: "please send this to a developer"});
+      if (err) response.status(401).send({error: "please, send this to a developer"});
   
       request.userId = decoded.id;
       next();
